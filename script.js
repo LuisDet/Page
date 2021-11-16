4389963e63805f9c4483159b2affd616a3bf7235
LuisDet <Luisendt@outlook.es>
2021-11-16 15:06:55 -0400

    head-tags

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const btn = document.querySelectorAll('.menu-item');
 const inputCheckbox = document.querySelector('#btn-menu');
-console.log(btn, inputCheckbox);
 btn.forEach((item) => {
   item.addEventListener('click', () => {
     inputCheckbox.checked = false;
